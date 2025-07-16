eb32f503e3c48963cc9c8fc964eef243620667d4
govind02420 <govind.02420@gmail.com>
2025-07-17 01:33:06 +0530

    19 Commit

--- test/test_app.py
+++ test/test_app.py
@@ -1,10 +1,10 @@
-# import sys
-# import os
+import sys
+import os
 
-# # Add the parent directory to the Python path
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
+import app
 
 def test_home():
     tester = app.app.test_client()

48fbd94f91edc0cb9a43c8487c99b5649d5a1df6
govind02420 <govind.02420@gmail.com>
2025-07-17 01:35:00 +0530

    20 Commit

--- test/test_app.py
+++ test/test_app.py
@@ -10,6 +10,15 @@ def test_home():
     tester = app.app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
+
+#------------------------------------
+
+# import app
+
+# def test_home_page():
+#     response = app.app.test_client().get('/')
+#     assert response.status_code == 200
+
 #------------------------------------
 
 # import app
@@ -18,14 +27,3 @@ def test_home():
 #     assert app.home() == 'Hello, Simple Python web application!'
 #------------------------------------
 
-# import sys
-# import os
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# import app
-#------------------------------------
-
-# import app
-
-# def test_home_page():
-#     response = app.app.test_client().get('/')
-#     assert response.status_code == 200

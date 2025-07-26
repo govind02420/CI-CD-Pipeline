8032c97fc4f2b52bd2c6420e1e7c73d6f5aa4d8a
govind02420 <govind.02420@gmail.com>
2025-07-26 13:49:47 +0530

    app and readme updated

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return 'Hello, Simple Python web application!'
 
+# Only run the app when executed directly (not during testing)
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Test function (can be discovered by pytest)
+def test_math():
+    assert 1 + 1 == 2
